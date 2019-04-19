58dd73d30a616bbc0cb1f9d2135be272093dee97
Kazuki Ota <kaota@microsoft.com>
2019-04-19 11:11:17 +0900

    add sample

---
+++ DurableFunctionsOrchestratorJS/index.ts
@@ -0,0 +1,25 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your 
+ *    function app in Kudu
+ */
+
+import * as df from "durable-functions"
+
+const orchestrator = df.orchestrator(function* (context) {
+    try {
+        const tasks = [];
+        tasks.push(context.df.callActivity("ErrorActivity"));
+        tasks.push(context.df.callActivity("ErrorActivity"));
+        return yield context.df.Task.all(tasks);
+    } catch (e) {
+        return "Error";
+    }
+});
+
+export default orchestrator;
